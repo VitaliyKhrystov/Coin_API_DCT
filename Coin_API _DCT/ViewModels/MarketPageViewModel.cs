1f48410d009997d3a3c3da2d0ad45f6dfa9f28db
Vitalii Khrystov <vitalik_khristov@ukr.net>
2022-09-20 04:36:23 +0300

    Created class MarketPageViewModel

---
+++ Coin_API _DCT/ViewModels/MarketPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coin_API__DCT.ViewModels
+{
+    public class MarketPageViewModel : ViewModelBase
+    {
+        public MarketPageViewModel(string coinId)
+        {
+
+        }
+    }
+}
